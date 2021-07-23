433da8eecb3595c24fd29e696057efdadbdc653f
iksflow <theiksflow@gmail.com>
2021-07-23 23:06:55 +0900

    learn class extend

---
+++ ch05/example_extend01.rb
@@ -0,0 +1,16 @@
+class Parent
+  def say_hello
+    puts "Hello from #{self}"
+  end
+end
+
+p = Parent.new
+p.say_hello
+
+# Generate child
+class Child < Parent
+
+end
+
+c = Child.new
+c.say_hello
