d6a46c97aaf4b4b661e5f14bd9860576e2ccc56d
iksflow <theiksflow@gmail.com>
2021-07-29 23:58:29 +0900

    learn array tips

--- ch02/array_example.rb
+++ ch02/array_example.rb
@@ -1,3 +1,10 @@
 array = [ 1, 'a', 3.14]
 puts "The first elements is #{array[0]}"
 
+starray = ['ant', 'bee', 'cat', 'dog', 'elk']
+puts starray[0]
+puts starray[2]
+
+starray2 = %w{ant bee cat dog elk}
+puts starray2[0]
+puts starray2[2]
