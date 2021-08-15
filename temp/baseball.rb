29c0c9e0ab39f46f7780376384e7d47cfc1e6414
iksflow <theiksflow@gmail.com>
2021-08-15 23:44:14 +0900

    baseball game

--- temp/baseball.rb
+++ temp/baseball.rb
@@ -5,9 +5,15 @@ end
 answer = "654"
 puts "숫자를 입력하세요."
 s = gets
+index = 0
 
 s.each_char.each do |number|
-  puts number
+  if answer[index] == number
+    puts "strike"
+  else
+    puts "out"
+  end
+  index += 1
 end
 
 puts "true" if "1" == "1"
